0d98a4c64a90e2e75181a73886412a4bdb25d6cd
apccc <abraham@apc.cc>
2017-03-20 23:26:11 -0700

    Update InstanceCreate.sh

--- scripts/buildAWS/InstanceCreate.sh
+++ scripts/buildAWS/InstanceCreate.sh
@@ -2,6 +2,6 @@
 source ~/setup_aws.conf.sh
 AMI="$DEFAULT_AMI"
 ITYPE='t2.micro'
-VMC=`~/setup_aws/scripts/tools/getControllerVPCId.sh`
-INSTANCEID=`aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$ITYPE" --key-name "$VMC" --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
+VPC=`~/setup_aws/scripts/tools/getControllerVPCId.sh`
+INSTANCEID=`aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$ITYPE" --key-name "$VPC" --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
 exit 0
