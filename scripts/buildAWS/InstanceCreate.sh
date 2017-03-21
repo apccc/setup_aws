3c00e372f20fe93eaa841ba4c779fffc9e028a44
apccc <abraham@apc.cc>
2017-03-20 23:24:25 -0700

    Update InstanceCreate.sh

--- scripts/buildAWS/InstanceCreate.sh
+++ scripts/buildAWS/InstanceCreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source
-AMI='ami-9eaa1cf6'
+source ~/setup_aws.conf.sh
+AMI="$DEFAULT_AMI"
 ITYPE='t2.micro'
 INSTANCEID=`aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$ITYPE" --key-name my-key --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
 exit 0
