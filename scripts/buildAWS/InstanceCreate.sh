b8040af1287ce78b3be48f079b58d159fd06dc77
apccc <abraham@apc.cc>
2017-03-21 00:02:58 -0700

    Making file executable.

--- scripts/buildAWS/InstanceCreate.sh
+++ scripts/buildAWS/InstanceCreate.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 source ~/setup_aws.conf.sh
-AMI="$DEFAULT_AMI"
-ITYPE='t2.micro'
+if [ -z "$1" ];then
+ ITYPE='t2.micro'
+else
+ ITYPE="$1"
+fi
+if [ -z "$2" ];then
+ AMIID="$DEFAULT_AMI"
+else
+ AMIID="$2"
+fi
 VPC=`~/setup_aws/scripts/tools/getControllerVPCId.sh`
 SECGRP="setupaws-sec-${VPC}-grp"
 SECGRPID=`~/setup_aws/scripts/tools/getSecurityGroupIdFromName.sh "$SECGRP"`
-SUBNETID=`~/setup_aws/scripts/tools/getCIDRBlockFromVPCId.sh "${VPC}"`
-INSTANCEID=`aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$ITYPE" --key-name "$VPC" --security-group-ids "$SECGRPID" --subnet-id "$SUBNETID" --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
+SUBNETID=`~/setup_aws/scripts/tools/getSubnetIDFromVPCID.sh "${VPC}"`
+INSTANCEID=`aws ec2 run-instances --image-id "$AMIID" --count 1 --instance-type "$ITYPE" --key-name "$VPC" --security-group-ids "$SECGRPID" --subnet-id "$SUBNETID" --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
+echo "$INSTANCEID"
 exit 0
