815149bd9d50e7b40a835e24220555a319b8241b
apccc <abraham@apc.cc>
2017-03-21 11:26:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,12 @@ if [ ! -f ~/setup_aws.conf.sh ];then
 else
   echo "Configuration file is already in place ~/setup_aws.conf.sh"
 fi
+
+if [ ! -f ~/ec2-metadata ];then
+  cd ~
+  wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+  chmod u+x ec2-metadata
+else
+  echo "EC2 Metadata File in Place!"
+fi
 exit 0
