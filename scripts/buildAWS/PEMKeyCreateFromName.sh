f991284c9918101e05e550b11b8d92c8a90068ac
apccc <abraham@apc.cc>
2017-03-20 22:20:50 -0700

    Making file executable.

---
+++ scripts/buildAWS/PEMKeyCreateFromName.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ -z "$1" ];then
+  echo "Key Name Not Set!"
+  exit 1
+fi
+KEYNAME="$1"
+KEYSTOREDIR="~/setup_aws_keystore"
+if [ ! -d "$D" ];then
+  mkdir "$D"
+fi
+F="${D}/${KEYNAME}.pem"
+if [ -f "$F" ];then
+  echo "Key already exists $F"
+fi
+aws ec2 create-key-pair --key-name "$KEYNAME" --query 'KeyMaterial' --output text > "$F"
+chmod 400 "$F"
+exit 0
