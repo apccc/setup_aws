4c1955bd565d36739661effbd95d096952383b01
apccc <abraham@apc.cc>
2017-03-20 23:43:25 -0700

    Update InstanceCreate.sh

--- scripts/buildAWS/InstanceCreate.sh
+++ scripts/buildAWS/InstanceCreate.sh
@@ -4,5 +4,7 @@ AMI="$DEFAULT_AMI"
 ITYPE='t2.micro'
 VPC=`~/setup_aws/scripts/tools/getControllerVPCId.sh`
 SECGRP="setupaws-sec-${VPC}-grp"
-INSTANCEID=`aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$ITYPE" --key-name "$VPC" --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
+SECGRPID=`~/setup_aws/scripts/tools/getSecurityGroupIdFromName.sh "$SECGRP"`
+SUBNETID=""
+INSTANCEID=`aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$ITYPE" --key-name "$VPC" --security-group-ids "$SECGRPID" --subnet-id "$SUBNETID" --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
 exit 0
