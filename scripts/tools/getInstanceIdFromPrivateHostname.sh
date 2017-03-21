93949ff7e6aab549311c4e98cd1e6222c2088796
apccc <abraham@apc.cc>
2017-03-21 15:38:51 -0700

    Update and rename getInstanceIdFromVPCAndPrivateHostname.sh to getInstanceIdFromPrivateHostname.sh

--- scripts/tools/getInstanceIdFromPrivateHostname.sh
+++ scripts/tools/getInstanceIdFromPrivateHostname.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+if [ -z "$1" ];then
+  exit 1
+fi
+ZHOSTNAME="$1"
+
+if [ -z "$2" ];then
+  VPCID=`~/setup_aws/scripts/tools/getControllerVPCId.sh`
+else
+  VPCID="$2"
+fi
+if [ -z "$VPCID" ];then
+  exit 1
+fi
 #get the instance id from the VPC and Private Hostname
 
 aws ec2 describe-instances --filters "Name=instance-type,Values=m1.small" "Name=availability-zone,Values=us-west-2c"
