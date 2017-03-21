c63ac6ddc2cf306798cdcd3de88ab83ac673282d
apccc <abraham@apc.cc>
2017-03-21 15:44:55 -0700

    Making file executable.

--- scripts/buildAWS/puppetSignVPCCerts.sh
+++ scripts/buildAWS/puppetSignVPCCerts.sh
@@ -2,5 +2,10 @@
 
 #sign certs from valid hostnames
 for ZHOSTNAME in `sudo /opt/puppetlabs/bin/puppet cert list | egrep -oe '"[a-z0-9.-]*"' | egrep -oe '[a-z0-9.-]*'`;do
-  echo "$ZHOSTNAME"
+  INSTANCEID=`~/setup_aws/scripts/tools/getInstanceIdFromPrivateHostnameAndVPC.sh "$ZHOSTNAME"`
+  if [ -z "$INSTANCEID" ];then
+    echo "$ZHOSTNAME not found, skipping!"
+    continue
+  fi
+  echo "Signing cert for instance $INSTANCEID with hostname $ZHOSTNAME"
 done
