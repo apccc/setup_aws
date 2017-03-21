3a7890493ebf176bba3c8bce6e2151a35dc79267
apccc <abraham@apc.cc>
2017-03-20 21:42:27 -0700

    Create VPCRouteExternalTrafficToGateway.sh

---
+++ scripts/buildAWS/VPCRouteExternalTrafficToGateway.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ -z "$1" ];then
+  echo "VPC Id not set!"
+  exit 0
+fi
+if [ -z "$2" ];then
+  echo "Subnet Id not set!"
+  exit 0
+fi
+if [ -z "$3" ];then
+  echo "Gateway Id not set!"
+  exit 0
+fi
+VPCID="$1"
+SUBNETID="$1"
+GATEWAYID="$1"
+ROUTETABLEID=`aws ec2 create-route-table --vpc-id "$VPCID" --query 'RouteTable.RouteTableId' --output text`
+aws ec2 associate-route-table --route-table-id "$ROUTETABLEID" --subnet-id "$SUBNETID"
+aws ec2 create-route --route-table-id "$ROUTETABLEID" --destination-cidr-block 0.0.0.0/0 --gateway-id "$GATEWAYID"
+
+exit 0
