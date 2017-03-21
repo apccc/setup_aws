6caa5df83e571abf41e4d25eeda1a342d496a65c
apccc <abraham@apc.cc>
2017-03-21 11:27:47 -0700

    Update whatsmyip.sh

--- scripts/tools/whatsmyip.sh
+++ scripts/tools/whatsmyip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ifconfig -a | grep 'inet addr' | grep -v '127.0.0.1' | tr -d '[:space:]' | cut -d':' -f2 | egrep -oe [0-9.]+
+~/ec2-metadata -v | cut -d':' -f2 | egrep -oe '[0-9.]+'
 exit 0
