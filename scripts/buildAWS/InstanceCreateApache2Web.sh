8b4dea17efe096ab06d37c3ce775c30dac4d22be
apccc <abraham@apc.cc>
2017-03-22 10:17:31 -0700

    Update InstanceCreateApache2Web.sh

--- scripts/buildAWS/InstanceCreateApache2Web.sh
+++ scripts/buildAWS/InstanceCreateApache2Web.sh
@@ -2,3 +2,11 @@
 
 #create an Apache 2 Web Instance
 
+INSTANCEID=`~/setup_aws/scripts/buildAWS/InstanceCreate.sh`
+if [ -z "$INSTANCEID" ];then
+  echo "Instance Failed to be created"
+  exit 1
+fi
+
+
+exit 0
