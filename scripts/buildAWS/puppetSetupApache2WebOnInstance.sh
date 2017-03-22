a36d671eb2a83a5cf393d9a4c43c2cf5dbc8a75c
apccc <abraham@apc.cc>
2017-03-21 19:11:27 -0700

    Update puppetSetupApache2WebOnInstance.sh

--- scripts/buildAWS/puppetSetupApache2WebOnInstance.sh
+++ scripts/buildAWS/puppetSetupApache2WebOnInstance.sh
@@ -3,10 +3,14 @@ if [ -z "$1" ];then
   echo "Instance Id Not Set!"
   exit 0
 fi
+INSTANCEID="$1"
+echo "Setting up Apache2 Web Server on Instance $INSTANCEID"
+INSTANCEURL=`~/setup_aws/scripts/tools/getInstanceURLFromId.sh $INSTANCEID`
+if [ -z "$INSTANCEURL ];then
+  echo "Instance URL Could Not Be Obtained!"
+  exit 0
+fi
 
-
-echo "Setting up Apache2 Web Server on Instance"
-echo ""
 sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apache
 
 exit 0
