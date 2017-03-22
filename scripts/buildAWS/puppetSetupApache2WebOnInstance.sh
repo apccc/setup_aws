52368754b546adbbb16e910ca181e73ea7dda521
apccc <abraham@apc.cc>
2017-03-21 19:24:24 -0700

    Making file executable.

--- scripts/buildAWS/puppetSetupApache2WebOnInstance.sh
+++ scripts/buildAWS/puppetSetupApache2WebOnInstance.sh
@@ -11,6 +11,30 @@ if [ -z "$INSTANCEURL ];then
   exit 0
 fi
 
+source ~/setup_aws.conf.sh
+
+#get the instance private hostname
+PRIVATEHOSTNAME=`~/setup_aws/scripts/tools/getInstancePrivateHostnameFromId.sh "$INSTANCEID"`
+
+#setup the puppet apache module
 sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apache
+
+#setup the puppet site file
+F="/etc/puppetlabs/code/environments/production/manifests/site.pp"
+
+#setup the apache entry for the file
+if [[ `grep "$PRIVATEHOSTNAME" "$F" | grep 'apache' | wc -l` -lt 1 ]];then
+  echo "node '${PRIVATEHOSTNAME}' { class { 'apache': }" | sudo tee -a "$F"
+  echo " apache::vhost { 'example.com':" | sudo tee -a "$F"
+  echo " port    => '80'," | sudo tee -a "$F"
+  echo " docroot => '/var/www/html'" | sudo tee -a "$F"
+  echo "}}" | sudo tee -a "$F"
+fi
+
+#make sure the default node entry is in place
+X="node default {}"
+if [[ `grep "$X" "$F" | wc -l` -lt 1 ]];then
+  echo "$X" | sudo tee -a "$F"
+fi
 
 exit 0
