8f93dd36e449846ea2fcae562f27024fe7872d59
apccc <abraham@apc.cc>
2017-04-06 20:04:00 -0700

    Making file executable.

---
+++ scripts/tools/mysql/commandServer1Link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ~/setup_aws.conf.sh
+
+if [[ ! $1 ]];then
+  echo '* mysql command server 1 link error - command not set'
+  exit 0
+fi
+
+echo "$1" | mysql -u root -p$MYSQL_ROOT_PW --host "$MYSQL_1_HOST"
