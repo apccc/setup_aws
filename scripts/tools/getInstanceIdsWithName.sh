bfbd3dd0234e69d244c666f2b87d2b1e60c942ef
apccc <abraham@apc.cc>
2017-04-15 10:50:10 -0700

    Update getInstanceIdsWithName.sh

--- scripts/tools/getInstanceIdsWithName.sh
+++ scripts/tools/getInstanceIdsWithName.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-#get the instance ids with a certain name
+if [ -z "$1" ];then
+  exit 1
+fi
 
+#get the instance ids with a certain name
+aws ec2 describe-instances --filters Name=tag:Name,Values="$1" --output text --query 'Reservations[*].Instances[*].{Name:InstanceId}' --output text
 exit 0
