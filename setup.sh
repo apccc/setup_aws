dcd217a1f48f8eae7c06ebfacd79b439a17afe37
Abraham P Chaffin <abraham@apc.cc>
2017-04-19 14:09:10 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+#setup the configuration file
 if [ ! -f ~/setup_aws.conf.sh ];then
-  echo "Copying the template settings file to ~/setup_aws.conf.sh"
-  echo "Configure this file to your liking!"
+  echo " * Copying the template settings file to ~/setup_aws.conf.sh"
+  echo " * Configure this file to your liking!"
   cp ~/setup_aws/settings/template.sh ~/setup_aws.conf.sh
 else
-  echo "Configuration file is already in place ~/setup_aws.conf.sh"
+  echo " * Configuration file is already in place ~/setup_aws.conf.sh"
 fi
 
 if [ ! -f ~/ec2-metadata ];then
@@ -13,10 +14,11 @@ if [ ! -f ~/ec2-metadata ];then
   wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
   chmod u+x ec2-metadata
 else
-  echo "EC2 Metadata File in Place!"
+  echo " * EC2 Metadata File in Place!"
 fi
 
 #install some packages
-sudo apt-get update
-sudo apt-get install -y expect ntp pcregrep
+echo " * Updating apt-get"
+sudo apt-get -qq update
+sudo apt-get -qq install -y expect ntp pcregrep
 exit 0
