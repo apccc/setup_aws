d2ec975d084d3917734c1a81176eba3122d31bd8
Abraham P Chaffin <abraham@apc.cc>
2017-05-20 01:16:46 -0700

    Making file executable

--- scripts/tools/connectToInstanceId.sh
+++ scripts/tools/connectToInstanceId.sh
@@ -12,10 +12,25 @@ else
 fi
 
 INSTANCEURL=`~/setup_aws/scripts/tools/getInstanceURLFromId.sh "$INSTANCEID"`
-
 if [ -z "$INSTANCEURL" ];then
   echo "Instance URL Could Not Be Obtained!"
   exit 1
 fi
+
+VPC=`~/setup_aws/scripts/tools/getControllerVPCId.sh`
+if [ -z "$VPC" ];then
+  echo "VPC Could Not Be Obtained!"
+  exit 1
+fi
+
+KEYF="~/setup_aws_keystore/${VPC}.pem"
+if [ -z "$KEYF" ];then
+  echo "Key File Could Not Be Found!"
+  exit 1
+fi
+
+echo "Connecting to $INSTANCEURL as $ZUSER"
+ssh -i "$KEYF" ${ZUSER}@${INSTANCEURL}
+echo "Disconnected from $INSTANCEURL"
 
 exit 0
